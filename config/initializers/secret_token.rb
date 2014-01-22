bae4c6f00e241f001b91b25d8a95b21c7056cdb8
John Johnson <dampboxdev@gmail.com>
2014-01-22 11:46:20 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Auth::Application.config.secret_token = '28eec30cd434ef545fc817eaed75c431df29cd9fb563a8291b17fb14dfd5b6a939a881ca647d5a032d6b6c838eae3e0b316520f6410ef1fcd8e478d707b9bb0f'
